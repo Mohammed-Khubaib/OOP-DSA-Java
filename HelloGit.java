e92f752e3aa59d0900cfacde5632970b7976d852
Rehan <zealrehan@gmail.com>
2022-03-27 01:14:26 +0530

    Modified

--- HelloGit.java
+++ HelloGit.java
@@ -1,5 +1,6 @@
 class HelloGit {
     public static void main(String[] args) {
-        System.out.println("hello git ");
+        System.out.println("Hello,using git");
+	System.out.println("First repo!");
     }
 }
